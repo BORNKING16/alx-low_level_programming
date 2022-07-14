1772c68fe69a3a7c1b43bbf685c4b9ff3e2f1051
BORNKING16 <ige.adeyemi1993@gmail.com>
2022-07-14 05:20:30 -0700

    kkk

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,32 +1,59 @@
-#include "coding.h"
+#include "main.h"
 
 /**
- * infinite_add - function with 4 arguments
- * @n1: char type pointer argument
- * @n2: char type pointer argument
- * @r: char type pointer argument
- * @size_r: int type argument
- * Description: adds two numbers from string
- * Return: sum of two integers
+ * infinite_add - add 2 integers.
+ * @n1: integer
+ * @n2: integer
+ * @r: buffer
+ * size_r: size of r
+ * Return: char
  */
+int _atoi(char *s)
+{
+	int sign = 1, resp = 0, firstNum;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+	return (sign * resp);
+}
+
+void int_to_string(int n)
+{
+int divisor = 1, i, resp;
+
+for (i = 0; n / divisor > 9; i++)
+{
+	divisor *= 10;
+}
+
+char str[i];
+
+for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
+{
+	resp = n / divisor;
+	str[cmpt] = '0' + resp;
+	n = n - resp * divisor;
+}
+str[i] = ('0' + n);
+
+}
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int count, count2;
+	int sum, a, b;
+   	a = _atoi(n1);
+	b = _atoi(n2);
 
-	while (n1[count] != '\0')
-		count++;
-	while (n2[count2] != '\0')
-		count2++;
-
-	*r = *(r + size_r);
-	while (n1[count] > 0 || n1[count2] > 0)
-	{
-		if (n1[count] + n2[count2] > 0)
-			*r = n1[count - 1] + n2[count2 - 1] + 1;
-		else
-			*r = n1[count] + n2[count2];
-		count--;
-		count2--;
-	}
-	return (r);
+	sum = a + b;
 }
