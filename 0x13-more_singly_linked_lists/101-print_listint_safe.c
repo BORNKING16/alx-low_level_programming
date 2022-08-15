11aad0961f0f23f83b52ca172f8e2a5a5ffcc0fb
BORNKING16 <ige.adeyemi1993@gmail.com>
2022-08-15 11:42:31 -0700

    i hope so

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,34 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
+ * find_listint_loop - finds the loop in a linked list
+ * @head: linked list to search for
  *
- * Return: number of nodes in the list
+ * Return: address of the node where the loop starts, or NULL
  */
-size_t print_listint_safe(const listint_t *head)
+listint_t *find_listint_loop(listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-	while (head)
+	if (!head)
+		return (NULL);
+
+	while (slow && fast && fast->next)
 	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			slow = head;
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (fast);
 		}
 	}
 
-	return (num);
+	return (NULL);
 }
